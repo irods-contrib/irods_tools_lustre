b64a1b293e583a1e2196719dec12dedb2e4b51f7
JustinKJames <jjames@renci.org>
2017-05-31 14:52:38 -0400

    changes from code review

---
+++ lustre_irods_connector/src/config.hpp
@@ -0,0 +1,27 @@
+#ifndef _IRODS_LUSTRE_CHANGELOG_CONFIG_H
+#define _IRODS_LUSTRE_CHANGELOG_CONFIG_H
+
+const int MAX_CONFIG_VALUE_SIZE = 256;
+
+typedef struct lustre_irods_connector_cfg {
+    char mdtname[MAX_CONFIG_VALUE_SIZE];
+    char lustre_root_path[MAX_CONFIG_VALUE_SIZE];
+    char irods_register_path[MAX_CONFIG_VALUE_SIZE];
+    char irods_resource_name[MAX_CONFIG_VALUE_SIZE];
+    int64_t irods_resource_id;
+    unsigned int changelog_poll_interval_seconds;
+    unsigned int update_irods_interval_seconds;
+} lustre_irods_connector_cfg_t;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int read_config_file(const char *filename, lustre_irods_connector_cfg_t *config_struct);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lustre_irods_connector/src/irods_ops.hpp
@@ -0,0 +1,19 @@
+#ifndef __irods_ops_hpp
+#define __irods_ops_hpp
+
+#include "rodsType.h"
+#include "inout_structs.h"
+#include "config.hpp"
+#include "rodsClient.h"
+
+class lustre_irods_connection {
+ public:
+   rcComm_t *irods_conn;
+   lustre_irods_connection() : irods_conn(nullptr) {}
+   ~lustre_irods_connection(); 
+   int send_change_map_to_irods(irodsLustreApiInp_t *inp) const;
+   int populate_irods_resc_id(lustre_irods_connector_cfg_t *config_struct_ptr);
+   int instantiate_irods_connection();
+};
+
+#endif
